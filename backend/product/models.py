0e353f243f92da3a0eef317eb90bcf15baec0062
shafee <shafeelinks@gmail.com>
2024-10-29 09:22:23 +0000

    increase field lengths for PostgreSQL compatibility

--- backend/product/models.py
+++ backend/product/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
-
 class Product(models.Model):
-    name = models.CharField(max_length=200, blank=False, null=False)
+    name = models.CharField(max_length=255, blank=False, null=False)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.BooleanField(default=False)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to='products/')
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
