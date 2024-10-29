4499314d54123f06d09acbea4392c1ec640f5c8b
Shafee <shafeelinks@gmail.com>
2024-10-29 00:52:47 +0000

    remove aws secret and aws key from vars

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,8 +1,6 @@
 # Configure AWS Provider
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  region = var.aws_region
 }
 
 # Data source to get the default VPC
--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,15 +1,4 @@
 # AWS Credentials
-variable "aws_access_key" {
-  description = "AWS Access Key"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_secret_key" {
-  description = "AWS Secret Key"
-  type        = string
-  sensitive   = true
-}
 
 variable "aws_region" {
   description = "AWS Region"
